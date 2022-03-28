e7c80e75c9a75622e845048dbb4597ef976d679a
ebubekir durbak <bkrdrbk1994@gmail.com>
2022-03-28 12:00:07 -0700

    fourth commit

---
+++ ebubekirdurbak/src/text4.java
@@ -0,0 +1,7 @@
+public class text4 {
+    public static void main(String[] args) {
+        System.out.println("/* ask olmak guzel bir duygu yasamak lazim lakin aci cekerek degil" +
+                "guzel duygulara sahip olarak yasamak" +
+                "kutretli bir assik sevdige guven birisidi */");
+    }
+}
